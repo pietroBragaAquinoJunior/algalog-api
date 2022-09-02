abef5fb4ddd87bd44390f7d5110b529da829b95e
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-09-01 22:32:32 -0300

    Criando controlador e usando entityManager para realizar consultas simples ao repositorio

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,15 +1,25 @@
 package com.algaworks.algalog.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
-    private String email;
+
+    @Column(name = "fone")
     private String telefone;
 
 }
